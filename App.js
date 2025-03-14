e2b890f44e8f56389b1a631c92991bd895a22b7b
gabriel-augusto28 <162993266+gabriel-augusto28@users.noreply.github.com>
2025-03-14 10:21:55 -0300

    App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return(
       <ScrollView>
         <View>
-          <Text style={{color: '#1C1C1C', fontSize: 25, margin: 10, fontFamily: 'Open Sans', fontOpticalSizing: 'auto', fontWeight: 'normal', fontStyle: 'normal', fontVariationSettings: '"wdth" 100'}}>
+          <Text style={{color: '#1C1C1C', fontSize: 25, margin: 10, fontFamily: 'Open Sans', fontWeight: 'normal', textAlign: 'center'}}>
             Auxiliar de treino
           </Text>
           <Text style={{
@@ -14,40 +14,44 @@ class App extends Component {
               android: 'Open Sans',
               ios: 'Open Sans',
             }),
-            fontOpticalSizing: 'auto',
             fontWeight: 'normal',
-            fontStyle: 'normal',
-            fontVariationSettings: '"wdth" 100'
+            textAlign: 'center'
           }}>
             Exercícios 
           </Text>
 
-          <Image
-            source={{uri: 'https://static.strengthlevel.com/images/exercises/bench-press/bench-press-400.avif'}}
-            style={{width: 250, height: 250}}
-          />
-          <Text style={{ fontFamily: 'Open Sans', fontOpticalSizing: 'auto', fontWeight: 'normal', fontStyle: 'normal', fontVariationSettings: '"wdth" 100'}}>
-            Supino reto com barra
-          </Text>
-          <Text style={{ fontFamily: 'Open Sans', fontOpticalSizing: 'auto', fontWeight: 'normal', fontStyle: 'normal', fontVariationSettings: '"wdth" 100'}}>
-            Músculo recrutado: Peitoral
-          </Text>
-          <Text style={{ fontFamily: 'Open Sans', fontOpticalSizing: 'auto', fontWeight: 'normal', fontStyle: 'normal', fontVariationSettings: '"wdth" 100'}}>
-            Tipo do exercício: Peso livre 
-          </Text>
-          <Image
-            source={{uri: 'https://static.strengthlevel.com/images/exercises/incline-bench-press/incline-bench-press-800.jpg'}}
-            style={{width: 250, height: 250, marginTop: 10}}
-          />
-          <Text style={{ fontFamily: 'Open Sans', fontOpticalSizing: 'auto', fontWeight: 'normal', fontStyle: 'normal', fontVariationSettings: '"wdth" 100'}}>
-            Supino inclinado com barra
-          </Text>
-          <Text style={{ fontFamily: 'Open Sans', fontOpticalSizing: 'auto', fontWeight: 'normal', fontStyle: 'normal', fontVariationSettings: '"wdth" 100'}}>
-            Músculo recrutado: Peitoral
-          </Text>
-          <Text style={{ fontFamily: 'Open Sans', fontOpticalSizing: 'auto', fontWeight: 'normal', fontStyle: 'normal', fontVariationSettings: '"wdth" 100'}}>
-            Tipo do exercício: Peso livre 
-          </Text>
+          <View style={{ borderWidth: 1, borderColor: '#ccc', borderRadius: 10, padding: 10, margin: 10, alignItems: 'center' }}>
+            <Image
+              source={{uri: 'https://static.strengthlevel.com/images/exercises/bench-press/bench-press-400.avif'}}
+              style={{width: 250, height: 250}}
+            />
+            <Text style={{ fontFamily: 'Open Sans', fontWeight: 'normal', textAlign: 'center'}}>
+              Supino reto com barra
+            </Text>
+            <Text style={{ fontFamily: 'Open Sans', fontWeight: 'normal', textAlign: 'center'}}>
+              Músculo recrutado: Peitoral
+            </Text>
+            <Text style={{ fontFamily: 'Open Sans', fontWeight: 'normal', textAlign: 'center'}}>
+              Tipo do exercício: Peso livre 
+            </Text>
+          </View>
+
+          <View style={{ borderWidth: 1, borderColor: '#ccc', borderRadius: 10, padding: 10, margin: 10, alignItems: 'center' }}>
+            <Image
+              source={{uri: 'https://static.strengthlevel.com/images/exercises/incline-bench-press/incline-bench-press-800.jpg'}}
+              style={{width: 250, height: 250, marginTop: 10}}
+            />
+            <Text style={{ fontFamily: 'Open Sans', fontWeight: 'normal', textAlign: 'center'}}>
+              Supino inclinado com barra
+            </Text>
+            <Text style={{ fontFamily: 'Open Sans', fontWeight: 'normal', textAlign: 'center'}}>
+              Músculo recrutado: Peitoral
+            </Text>
+            <Text style={{ fontFamily: 'Open Sans', fontWeight: 'normal', textAlign: 'center'}}>
+              Tipo do exercício: Peso livre 
+            </Text>
+          </View>
+
           <Jobs largura={250} altura={250}/>
         </View>
       </ScrollView>
@@ -63,18 +67,18 @@ class Jobs extends Component {
     let img = 'https://static.strengthlevel.com/images/exercises/machine-chest-fly/machine-chest-fly-800.jpg';
     
     return(
-      <View>
+      <View style={{ borderWidth: 1, borderColor: '#ccc', borderRadius: 10, padding: 10, margin: 10, alignItems: 'center' }}>
         <Image
           source={{uri: img}}
           style={{width: this.props.largura, height: this.props.altura}}
         />
-        <Text style={{ fontFamily: 'Open Sans', fontOpticalSizing: 'auto', fontWeight: 'normal', fontStyle: 'normal', fontVariationSettings: '"wdth" 100'}}>
+        <Text style={{ fontFamily: 'Open Sans', fontWeight: 'normal', textAlign: 'center'}}>
           Nome do exercício: Crucifixo com máquina
         </Text>
-        <Text style={{ fontFamily: 'Open Sans', fontOpticalSizing: 'auto', fontWeight: 'normal', fontStyle: 'normal', fontVariationSettings: '"wdth" 100'}}>
+        <Text style={{ fontFamily: 'Open Sans', fontWeight: 'normal', textAlign: 'center'}}>
           Músculo recrutado: Peitoral
         </Text>
-        <Text style={{ fontFamily: 'Open Sans', fontOpticalSizing: 'auto', fontWeight: 'normal', fontStyle: 'normal', fontVariationSettings: '"wdth" 100'}}>
+        <Text style={{ fontFamily: 'Open Sans', fontWeight: 'normal', textAlign: 'center'}}>
           Tipo do exercício: Máquina 
         </Text>
       </View>
